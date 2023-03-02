56662f7a5414cdd86b66ea04009d68c3532a6ebf
Shahna-shifu <shahnasharinpp@gmail.com>
2023-03-02 17:23:15 +0530

    netflix

---
+++ src/constants/constants.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = 'ae6f4226d780abbdeada8871f64cf5b1';
+export const imageUrl ='https://image.tmdb.org/t/p/original';
---
+++ src/urls.jsx
@@ -0,0 +1,3 @@
+import {API_KEY} from './constants/constants'
+export const originals =`/discover/movie?api_key=${API_KEY}&with_genres=27`
+export const action=`/discover/movie?api_key=${API_KEY}&with_genres=28`
